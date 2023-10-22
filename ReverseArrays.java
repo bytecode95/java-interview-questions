2e1420b0d0438f28ff7e1a35395a8167ba5cf504
chethanavirajini <cheth.virajini@gmail.com>
2023-10-23 00:38:53 +0530

    ReverseArrays

---
+++ ReverseArrays.java
@@ -0,0 +1,10 @@
+class ReverseArrays{
+    public static void main(String[] args){
+        int[] ar = {25, 36, 76, 58, 89, 100, 76, 10, 5};
+
+        for(int i = ar.length-1;  i >=0; i--){
+            System.out.println(ar[i]);
+        }
+
+    }
+}
