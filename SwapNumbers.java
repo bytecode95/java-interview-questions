d987ecf43776c67c4bca05765be58c29a687d8b8
chethanavirajini <cheth.virajini@gmail.com>
2023-10-30 13:46:32 +0530

    Swap Numbers

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,5 +1,18 @@
 public class SwapNumbers {
-    public static void main(){
-        
+    public static void main(String[] args ){
+        // Declare and initialize two numbers
+        int a = 5;
+        int b = 10;
+
+        // Display the original values
+        System.out.println("Original values: a = " + a + ", b = " + b);
+
+        // Swap without using a temporary variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        // Display the values after swapping
+        System.out.println("Values after swapping: a = " + a + ", b = " + b);
     }
 }
