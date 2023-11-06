9b585bc1619632854051e67b2d1721db357323b8
chethanavirajini <cheth.virajini@gmail.com>
2023-11-06 17:59:02 +0530

    Check Vowel in word

--- CheckVowel.java
+++ CheckVowel.java
@@ -6,13 +6,22 @@ public class CheckVowel {
         System.out.print("Enter word: ");
         String str = sc.next();
         str = str.toLowerCase();
+        
+        if(checkVowel(str)){
+            System.out.println("Word is having at least one Vowel ");
+        }else{
+            System.out.println("Word is not having at least one Vowel ");
+        }
+        
+    }
+
+    public static boolean checkVowel(String str){
         char[] ch = str.toCharArray();
         for(int i=0; i<ch.length; i++){
             if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'){
-                System.out.println(" is having a at least one Vowel");
-            }else{
-                System.out.println(" is not having a Vowel");
+                return true;
             }
         }
+        return false;
     }
 }
