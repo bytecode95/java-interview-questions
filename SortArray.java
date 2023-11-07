98b39afde4a70376ee2ff6230e417f4ec0367112
chethanavirajini <cheth.virajini@gmail.com>
2023-11-07 23:38:38 +0530

    array sorting

---
+++ SortArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args){
+        int[] nums = {3,2,6,8,9,5,-8,-3,12,15};
+        //using inbuit method
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
